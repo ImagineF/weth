c19a0b306ebe7d11bed2eb69b18fd27f78314978
ImagineF <104614298+ImagineF@users.noreply.github.com>
2023-02-21 19:19:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -165,6 +165,10 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
                 "value": love_days,
                 "color": get_color()
             },
+            "keywords": {
+                "value": keywords,
+                "color": get_color()
+            },
             "note_en": {
                 "value": note_en,
                 "color": get_color()
