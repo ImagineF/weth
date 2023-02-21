9751224e2559f2b47cf56dedaaeaff3e2e8e0cd2
ImagineF <104614298+ImagineF@users.noreply.github.com>
2023-02-21 19:25:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -231,9 +231,7 @@ if __name__ == "__main__":
     keywords = config["keywords"]
     if note_ch == "" and note_en == "":
         # 获取词霸每日金句
-        note_ch, note_en = get_ciba()
-       if keywords =="":
-        keywords = keywords()
+        note_ch, note_en = get_ciba(),
     # 公众号推送消息
     for user in users:
         send_message(user, accessToken, region, weather, temp, wind_dir, keywords, note_ch, note_en)
